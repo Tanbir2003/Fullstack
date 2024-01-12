8126e0f0de18398c8cfdaa5f07ea59c40e134b9a
Tanbir2003 <119125758+Tanbir2003@users.noreply.github.com>
2024-01-13 00:31:26 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,58 @@
+5+3
+let x=10;
+x*2
+let message
+let message1="Hello,";
+let name="world,";
+message1+name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function greet(name){
+return "Hello,"+name+ "!";
+}
+greet("Alice")
+let num=7;
+console.log("greater than 5");
+let num1=7;
+if(num>5){
+console.log("greater than 5");
+}
+else{
